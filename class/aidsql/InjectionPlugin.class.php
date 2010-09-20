d8e45b6f177f80c7b4dc4c4195244b079fd35bd7
jpfstange <jpfstange@5380c68a-0729-085b-3d97-e608d9370013>
2010-09-20 20:24:42 +0000

    Modular Parser compliance added

--- class/aidsql/InjectionPlugin.class.php
+++ class/aidsql/InjectionPlugin.class.php
@@ -1,92 +1,190 @@
 <?php
 
-	abstract class InjectionPlugin implements InjectionPluginInterface {
+	namespace aidSQL\plugin {
 
-		private $_stringEscapeCharacter			= NULL;
-		private $_queryConcatenationCharacter	= NULL;
-		private $_queryCommentOpen					= NULL;
-		private $_queryCommentClose				= NULL;
-		private $_table								= NULL;
-		private $_httpAdapter						= NULL;
+		abstract class InjectionPlugin implements InjectionPluginInterface {
 
-		public function setStringEscapeCharacter($escape=NULL){
+			private $_stringEscapeCharacter			= NULL;
+			private $_queryConcatenationCharacter	= NULL;
+			private $_queryCommentOpen					= NULL;
+			private $_queryCommentClose				= NULL;
+			private $_table								= NULL;
+			private $_verbose								= FALSE;
+			protected $_httpAdapter						= NULL;
+			protected $_affectedVariable				= Array();
+			protected $_injectionAttempts				= 100;
+			protected $_parser							= NULL;
 
-			if(empty($escape)){
-				throw(new Exception("String escape character cannot be empty!"));
+			public final function __construct(\HttpAdapter $adapter){
+
+				if(!$adapter->getRequestVariables()){
+					throw(new \Exception("Unable to perform injection without any request variables set in the http adapter!"));
+				}
+
+				$this->_httpAdapter = $adapter;
+
 			}
 
-			$this->_stringEscapeCharacter = $escape;
+			/**
+			*Good for decoupling execution with injection string generation
+			*/
 
-		}
+			public function execute($variable,$value){
 
-		public function setHttpAdapter(HttpAdapter $adapter){
-			$this->_httpAdapter = $adapter;
-		}
+				$this->_httpAdapter->addRequestVariable($variable,$value);
+				echo "Fetching ".$this->_httpAdapter->getFullUrl()."\n\n";
+				$content = $this->_httpAdapter->fetch();
 
-		public function getHttpAdapter(){
-			return $this->_httpAdapter;
-		}
+				if($this->_verbose){
+					echo $content;
+				}
 
-		public function setTable($table=NULL){
+				return $content;
 
-			if(empty($table)){
-				throw (new Exception("The table name cant be empty!"));
 			}
 
-			$this->_table = $table;
+			public function setStringEscapeCharacter($escape=NULL){
 
-		}
+				if(empty($escape)){
+					throw(new Exception("String escape character cannot be empty!"));
+				}
 
-		public function getTable(){
-			return $this->_table;
-		}
+				$this->_stringEscapeCharacter = $escape;
 
-		public function getStringEscapeCharacter(){
-
-			return $this->_stringEscapeCharacter;
-
-		}
-
-		public function setQueryConcatenationCharacter($concatChar=NULL){
-
-			if(empty($concatChar)){
-				throw(new Exception("String escape character cannot be empty!"));
 			}
 
-			$this->_queryConcatenationCharacter = $concatChar;
-
-		}
-
-		public function getQueryConcatenationCharacter(){
-			return $this->_queryConcatenationCharacter;
-		}
-
-		public function setQueryCommentOpen($commentOpen=NULL){
-
-			if(empty($concatChar)){
-				throw(new Exception("Query comment open character cannot be empty!"));
+			public function setHttpAdapter(HttpAdapter $adapter){
+				$this->_httpAdapter = $adapter;
 			}
 
-			$this->_queryCommentOpen = $commentOpen;
-
-		}
-
-		public function getQueryCommentOpen(){
-			return $this->_queryCommentOpen;
-		}
-
-		public function setQueryCommentClose($commentClose=NULL){
-
-			if(empty($commentClose)){
-				throw(new Exception("Query comment close character cannot be empty!"));
+			public function getHttpAdapter(){
+				return $this->_httpAdapter;
 			}
 
-			$this->_queryCommentClose = $commentClose;
+			public function setTable($table=NULL){
 
-		}
+				if(empty($table)){
+					throw (new Exception("The table name cant be empty!"));
+				}
 
-		public function getQueryCommentClose(){
-			return $this->_queryCommentClose;
+				$this->_table = $table;
+
+			}
+
+			public function getTable(){
+				return $this->_table;
+			}
+
+			public function getStringEscapeCharacter(){
+
+				return $this->_stringEscapeCharacter;
+
+			}
+
+			public function setQueryConcatenationCharacter($concatChar=NULL){
+
+				if(empty($concatChar)){
+					throw(new Exception("String escape character cannot be empty!"));
+				}
+
+				$this->_queryConcatenationCharacter = $concatChar;
+
+			}
+
+			public function getQueryConcatenationCharacter(){
+				return $this->_queryConcatenationCharacter;
+			}
+
+			public function setQueryCommentOpen($commentOpen=NULL){
+
+				if(empty($concatChar)){
+					throw(new Exception("Query comment open character cannot be empty!"));
+				}
+
+				$this->_queryCommentOpen = $commentOpen;
+
+			}
+
+			public function getQueryCommentOpen(){
+				return $this->_queryCommentOpen;
+			}
+
+			public function setQueryCommentClose($commentClose=NULL){
+
+				if(empty($commentClose)){
+					throw(new Exception("Query comment close character cannot be empty!"));
+				}
+
+				$this->_queryCommentClose = $commentClose;
+
+			}
+
+			public function getQueryCommentClose(){
+				return $this->_queryCommentClose;
+			}
+
+			public function setAffectedVariable($var,$value){
+
+				$this->_affectedVariable = array("variable"=>$var,"value"=>$value);
+
+			}
+
+			public function getAffectedVariable(){
+				return $this->affectedVariable;
+			}
+
+
+			/**
+			*This for now, only stands for outputting the results from URL execution to stdout
+			*/
+
+			public function setVerbose($boolean=TRUE){
+				$this->_verbose = $boolean;
+			}
+
+			public function getVerbose(){
+				return $this->_verbose;
+			}
+
+			public function setParser(\aidSQL\parser\ParserInterface $parser){
+				$this->_parser = $parser;
+			}
+
+			public function getParser(){
+				return $this->_parser;
+			}
+
+
+			/**
+			 *	Performs directory listing on the parser directory to find out which parsers
+			 * are available.
+			 * @return Array Parsers as string
+			 */
+			public function listParsers(){
+
+					$dir	= __CLASSPATH."/parser/";
+					$dp	= opendir($dir);
+
+					$baseParser = "Generic.parser.php";
+					$parserList = array();
+
+					while($file = readdir($dp)){
+
+						if(is_dir($file)||$file==$baseParser||preg_match("/^[.]/",$file)){
+							continue;
+						}
+
+						$class = substr($file,0,strpos($file,"."));
+						$parserList[] = $class;
+
+					}
+
+					closedir($dp);
+
+					return $parserList;
+
+			}
+
 		}
 
 	}
