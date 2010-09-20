52a677653ec619001d34b5b0e64c385799bfff68
jpfstange <jpfstange@5380c68a-0729-085b-3d97-e608d9370013>
2010-09-20 20:34:43 +0000

    Modified several interfaces to match requirements

---
+++ trunk/interface/Parser.interface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace aidSQL\parser {
+
+	interface ParserInterface{
+
+		public function setContent($content);
+		public function getContent();
+		public function getResult();
+
+	}
+
+}
+
+?>
